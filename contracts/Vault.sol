1c406aeade696c518560be466942af9e6e064a67
re1ro <rma4ok@gmail.com>
2025-02-26 12:57:16 -0700

    feat(IntentSource): revert on failed token recovery (#193)

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -151,9 +151,7 @@ contract Vault is IVault {
      */
     function _recoverToken(address refundToken, address creator) internal {
         uint256 refundAmount = IERC20(refundToken).balanceOf(address(this));
-        if (refundAmount > 0) {
-            IERC20(refundToken).safeTransfer(creator, refundAmount);
-        }
+        IERC20(refundToken).safeTransfer(creator, refundAmount);
     }
 
     /**
