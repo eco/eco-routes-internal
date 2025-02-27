8f675af68dc7eac0de972e19025aa37c5ff895fd
Nishaad <nknavkal@gmail.com>
2025-02-27 02:54:36 -0700

    temp

--- contracts/interfaces/IStablePool.sol
+++ contracts/interfaces/IStablePool.sol
@@ -2,18 +2,29 @@
 pragma solidity ^0.8.20;
 
 interface IStablePool {
-  event TokenWhitelistChanged(address indexed token, bool allowed);
-  event Deposited(address indexed user, address indexed token, uint256 amount);
-  event Withdrawn(address indexed user, address indexed token, uint256 amount);
+    event TokenWhitelistChanged(address indexed token, bool allowed);
+    event Deposited(
+        address indexed user,
+        address indexed token,
+        uint256 amount
+    );
+    event Withdrawn(
+        address indexed user,
+        address indexed token,
+        uint256 amount
+    );
 
-  // Custom Errors for Gas Efficiency
-  error InvalidToken();
-  error InvalidAmount();
-  error InsufficientTokenBalance(address _token);
-  error TransferFailed();
+    // Custom Errors for Gas Efficiency
+    error InvalidToken();
+    error InvalidAmount();
+    error InsufficientTokenBalance(address _token);
+    error TransferFailed();
 
-  function setAllowedToken(address token, bool allowed) external;
-  function deposit(address token, uint256 amount) external;
-  function withdraw(address token, uint256 amount) external;
-  function getBalance(address user, address token) external view returns (uint256);
+    function setAllowedToken(address token, bool allowed) external;
+    function deposit(address token, uint256 amount) external;
+    function withdraw(address token, uint256 amount) external;
+    function getBalance(
+        address user,
+        address token
+    ) external view returns (uint256);
 }
